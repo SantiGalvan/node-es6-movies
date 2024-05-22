3987f4f511f513fa5a9b8e4b3d00a1bdf4874473
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-22 14:47:02 +0200

    created media.js and import into scrip.js

---
+++ media.js
@@ -0,0 +1,86 @@
+const media = [
+    {
+        title: "Il Signore degli Anelli: La Compagnia dell'Anello",
+        year: 2001,
+        genre: "Fantasia, Avventura",
+        rating: 8.8,
+        type: "movie",
+    },
+    {
+        title: "Il Padrino",
+        year: 1972,
+        genre: "Gangster, Drammatico",
+        rating: 9.2,
+        type: "movie",
+    },
+    {
+        title: "Breaking Bad",
+        year: 2008,
+        genre: "Crime, Drammatico",
+        rating: 9.5,
+        type: "tv",
+        seasons: 5,
+    },
+    {
+        title: "Game of Thrones",
+        year: 2011,
+        genre: "Fantasy, Drammatico",
+        rating: 9.2,
+        type: "tv",
+        seasons: 8,
+    },
+    {
+        title: "La Principessa Mononoke",
+        year: 1997,
+        genre: "Animazione, Fantasy, Avventura",
+        rating: 8.3,
+        type: "movie",
+    },
+    {
+        title: "Inception",
+        year: 2010,
+        genre: "Sci-Fi",
+        rating: 8.8,
+        type: "movie"
+    },
+    {
+        title: "Breaking Bad",
+        year: 2008,
+        genre: "Crime",
+        rating: 9.5,
+        type: "tv",
+        seasons: 5
+    },
+    {
+        title: "Il cavaliere oscuro",
+        year: 2008,
+        genre: "Azione",
+        rating: 9.0,
+        type: "movie"
+    },
+    {
+        title: "Stranger Things",
+        year: 2016,
+        genre: "Horror",
+        rating: 8.7,
+        type: "tv",
+        seasons: 4
+    },
+    {
+        title: "The Godfather",
+        year: 1972,
+        genre: "Crime",
+        rating: 9.2,
+        type: "movie"
+    },
+    {
+        title: "Friends",
+        year: 1994,
+        genre: "Comedy",
+        rating: 8.9,
+        type: "tv",
+        seasons: 10
+    }
+];
+
+module.exports = media;
