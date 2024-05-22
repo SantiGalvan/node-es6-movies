3c361453c8f1e5d544752505fa31aa762b262172
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-22 17:42:52 +0200

    added private properties, functions get and set and fixed functions

--- script.js
+++ script.js
@@ -3,30 +3,77 @@ const media = require('./media');
 
 // Classe Movie
 class Movie {
+    #title;
+    #year
+    #genre
+    #rating;
+    #type;
+
     constructor(title, year, genre, rating, type) {
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.rating = rating;
-        this.type = type;
+        this.#title = title;
+        this.#year = year;
+        this.#genre = genre;
+        this.#rating = rating;
+        this.#type = type;
     }
 
     // Funzione toString Movie
     toString() {
-        return (`${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}`);
+        return (`${this.#title} è un film di genere ${this.#genre}. E' stato rilasciato nel ${this.#year} ed ha un voto di ${this.#rating}`);
+    }
+
+    // Funzioni Get
+    getTitle() {
+        return this.#title;
+    }
+    getYear() {
+        return this.#year;
+    }
+    getGenre() {
+        return this.#genre;
+    }
+    getRating() {
+        return this.#rating;
+    }
+    getType() {
+        return this.#type;
+    }
+
+    // Funzioni Set
+    setTitle(title) {
+        return this.#title = title;
+    }
+    setYear(year) {
+        return this.#year = year;
+    }
+    setGenre(genre) {
+        return this.#genre = genre;
+    }
+    setRating(rating) {
+        return this.#rating = rating;
+    }
+    setType(type) {
+        return this.#type = type;
     }
 };
 
 // Classe TvSerie
 class TvSerie extends Movie {
+    #seasons
+
     constructor(title, year, genre, rating, type, seasons) {
         super(title, year, genre, rating, type);
-        this.season = seasons;
+        this.#seasons = seasons;
     }
 
     // Funzione toString TvSerie
     toString() {
-        return (`${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciato nel ${this.year} ed in totale sono state prodotte ${this.season} stagioni. Ha un voto di ${this.rating}`)
+        return (`${this.getTitle()} è una serie tv di genere ${this.getGenre()}. La prima stagione è stata rilasciato nel ${this.getYear()} ed in totale sono state prodotte ${this.getSeasons()} stagioni. Ha un voto di ${this.getRating()}`)
+    }
+
+    // Funzioni Get
+    getSeasons() {
+        return this.#seasons;
     }
 }
 
@@ -42,23 +89,23 @@ const classMedia = media.map(({ title, year, genre, rating, type, seasons }) => 
 // Funzione per la media dei voti
 const getAverageRaiting = (list, genre) => {
     const genreMedia = list.filter(l => {
-        if (l.genre.toLowerCase() === genre.toLowerCase()) return true
+        if (l.getGenre().toLowerCase() === genre.toLowerCase()) return true
     });
 
-    const totRaiting = genreMedia.reduce((sum, { rating }) => {
-        return sum = rating++;
+    const totRaiting = genreMedia.reduce((sum, m) => {
+        return sum = m.getRating() + m.getRating();
     }, 0);
 
     return genreMedia.length ? totRaiting / genreMedia.length : 'Media non disponibile';
 };
 
-console.log(getAverageRaiting(classMedia, 'fantasia'));
+console.log(getAverageRaiting(classMedia, 'crime'));
 
 // Funzione per la lista dei gemeri
 const getAllGenres = (list) => {
 
     const allGenres = list.reduce((result, l) => {
-        if (!result.includes(l.genre)) result.push(l.genre);
+        if (!result.includes(l.getGenre())) result.push(l.getGenre());
         return result
     }, [])
 
@@ -67,11 +114,10 @@ const getAllGenres = (list) => {
 
 console.log(getAllGenres(classMedia));
 
-
 // Funzione che filtri i film in base ad un genere passato come argomento e ne ritorni un array con all'interno il risultato della funzione toString() di ogni film.
 const getFilteredMedia = (list, genre) => {
     const filteredMedia = list.reduce((result, l) => {
-        if (l.genre.toLowerCase() === genre.toLowerCase()) result.push(l.toString());
+        if (l.getGenre().toLowerCase() === genre.toLowerCase()) result.push(l.toString());
         return result;
     }, []);
 
